3d63ac7fd1e3b0272c7ba321f11b22d70c4b4f51
lucas-nunes-matheus <lucas.matheus@sou.inteli.edu.br>
2025-03-08 14:17:16 -0300

    feat: adiciona documentacao da api com swagger

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,16 @@
 import express from "express";
 import userRoutes from "./routes/userRoutes";
+import { setupSwagger } from "./config/swagger";
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
-app.use('/users', userRoutes);
+app.use("/users", userRoutes);
+
+setupSwagger(app);
 
 app.listen(port, () => {
     console.log(`Servidor ouvindo a porta localhost ${port}`);
+    console.log(`Swagger UI disponível em http://localhost:${port}/docs`);
 })
